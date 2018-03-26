a2ed91a365fd83bcfad1f52ed602917056ef2aaa
yongk_liu <1194117884@qq.com>
2018-03-26 16:40:14 +0800

    初始化提交

---
+++ src/main/java/me/kkang/tools/bridge_server/BridgeServerApplication.java
@@ -0,0 +1,12 @@
+package me.kkang.tools.bridge_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BridgeServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BridgeServerApplication.class, args);
+	}
+}
